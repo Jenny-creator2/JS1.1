19bd0a25083b8c41e26da19f04444d4bb51ef61a
Jenny-creator2 <like.monkey3@gmail.com>
2024-12-10 11:32:34 +0300

    изменение 3

--- js/3.js
+++ js/3.js
@@ -1,13 +1,29 @@
 function solve() {
 
-    let a = Number(document.getElementById('a').value);
-    let b = Number(document.getElementById('b').value);
-    let c = Number(document.getElementById('c').value);
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
 
     if (isNaN(a) || isNaN(b) || isNaN(c)) {
         document.getElementById('result').textContent = 'Пожалуйста, введите числа.';
         return;
     }
+
+
+  if (a === 0) {
+    if (b === 0) {
+        if (c === 0) {
+            document.getElementById('result').textContent = 'У уравнения бесконечно много решений.';
+        } else {
+            document.getElementById('result').textContent = 'У уравнения нет решений.';
+        }
+    } else {
+        let x = -c / b;
+        document.getElementById('result').textContent = 'У уравнения одно решение';
+    }
+    return;
+}
+
 
 
     let discriminant = b * b - 4 * a * c;
